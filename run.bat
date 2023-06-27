d9f57fff02b9504841f986820aefcecf0c5b1410
anil-bhandi <anil.bhandi440@gmail.com>
2023-06-27 17:58:09 +0200

    bat file code commented

--- run.bat
+++ run.bat
@@ -1,4 +1,4 @@
 
-call %~dp0\ecom\Scripts\activate.bat
+@REM call %~dp0\ecom\Scripts\activate.bat
 pytest -v -m "regression"  --html=Reports/report.html testCase/test_login.py --browser chrome
 @REM pytest -v -m "sanity"  --html=Reports/report.html testCase/ --browser chrome
